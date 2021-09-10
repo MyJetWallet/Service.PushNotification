6af87eda387010aa6ffaecc0d8eea6516bb64afa
novichikhin alexey <novichikhin.au@gmail.com>
2021-09-10 13:15:34 +0300

    withdrawal decline

--- src/Service.PushNotification.Domain.Models/Enums/NotificationTypeEnum.cs
+++ src/Service.PushNotification.Domain.Models/Enums/NotificationTypeEnum.cs
@@ -6,6 +6,7 @@ namespace Service.PushNotification.Domain.Models.Enums
         TradeNotification,
         WithdrawalApprove,
         WithdrawalComplete,
+        WithdrawalDecline,
         DepositReceive,
         Swap
     }
