97d40e5ae438b8e29cb3dec632a34cffb05cca3d
Pavlo Pylypenko <pavel.p@smplt.net>
2022-05-12 12:36:13 +0300

    nuget update

--- src/Service.PushNotification/Program.cs
+++ src/Service.PushNotification/Program.cs
@@ -38,7 +38,7 @@ namespace Service.PushNotification
             Settings = SettingsReader.GetSettings<SettingsModel>(SettingsFileName);
             Console.WriteLine("Config is loaded");
 
-            using var loggerFactory = LogConfigurator.ConfigureElk("MyJetWallet", Settings.SeqServiceUrl, Settings.ElkLogs);
+            using var loggerFactory = LogConfigurator.ConfigureElk_v2("MyJetWallet", Settings.SeqServiceUrl, Settings.ElkLogs);
 
             var logger = loggerFactory.CreateLogger<Program>();
 
